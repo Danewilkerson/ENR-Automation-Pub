f8bc746e234d49c265636cee8a7d8b5c4d655197
Danewilkerson <53789206+Danewilkerson@users.noreply.github.com>
2020-09-13 10:15:35 -0500

    Add files via upload

---
+++ Pages/page_library.py
@@ -0,0 +1,56 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from Resources.test_data import TestData
+from Resources.locators import Locators
+import requests, unittest, time, os
+
+
+class BasePage():
+
+    def __init__(self, driver):
+        self.driver=driver
+
+    def click(self, by_locator):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator)).click()
+        time.sleep(.3)
+
+    def assert_element_text(self, by_locator, element_text):
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(by_locator))
+        assert element.get_attribute("textContent") == element_text
+
+    def assert_element_source(self, by_locator, element_source):
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
+        assert element.get_attribute("src") == element_source
+        
+    def assert_element_placeholder(self, by_locator):
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
+        assert element.get_attribute("placeholder") == "Search…"
+
+    def assert_element_is_displayed(self, by_locator):
+        element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator)).is_displayed()
+
+    def assert_element_size(self, by_locator, element_size):
+        element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(by_locator)).size
+        assert element == element_size
+
+    def assert_GET_status(self, request_url, expected_GET_status_code):
+        r = requests.get(request_url)
+        assert r.status_code == expected_GET_status_code
+        print(f"{request_url} recieved a GET status '200'")
+
+    def enter_text(self, by_locator, text):
+        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator)).send_keys(text)
+        
+    def wait_for_page_to_load(self):
+        page = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(Locators.PR_HEADER))
+
+
+class Search(BasePage):
+
+    def __init__(self, driver):
+        self.driver=driver
+
+    def search(self, text):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(Locators.SEARCH_TEXTBOX_PLACEHOLDER)).send_keys(text)

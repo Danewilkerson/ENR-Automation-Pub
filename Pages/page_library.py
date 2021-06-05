334917e5bbbec0acd6ea3aac23103008f5d63b37
Danewilkerson <dane.wilkerson@knowink.com>
2021-06-05 14:53:22 -0500

    testing allure steps in base file

--- Pages/page_library.py
+++ Pages/page_library.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from Resources.test_data import TestData
 from Resources.locators import Locators
 from utilities.customLogger import LogGen
-import requests, unittest, time, os
+import requests, unittest, time, os, allure
 
 
 class BasePage():
@@ -14,50 +14,61 @@ class BasePage():
     def __init__(self, driver):
         self.driver=driver
 
+    @allure.step(f"Click: {0}")
     def click(self, by_locator):
         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator)).click()
         time.sleep(.5)
 
+    @allure.step(f"Returning element text: {0}")
     def return_element_text(self, by_locator):
         element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(by_locator)).get_attribute("textContent")
         return element
 
+    @allure.step(f"Printing element text: {0}")
     def print_element_text(self, by_locator):
         element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(by_locator)).get_attribute("textContent")
         self.logger.info("Printed element text: " + element)
 
+    @allure.step(f"Asserting element text: {0} with {1}")
     def assert_element_text(self, by_locator, element_text):
         element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(by_locator))
         assert element.get_attribute("textContent") == element_text
         self.logger.info("Validation | Element text: " + element_text)
 
+    @allure.step(f"Asserting element source: {0}, {1}")
     def assert_element_source(self, by_locator, element_source):
         element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
         assert element.get_attribute("src") == element_source
         self.logger.info("Validation | Element source: " + element_source)
         
+    @allure.step(f"Asserting search box placeholder: {0} to equal 'Search...'")
     def assert_element_placeholder(self, by_locator):
         element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
         assert element.get_attribute("placeholder") == "Search…"
         self.logger.info('Validation | Search placeholder is visible and text is: "Search…"')
 
+    @allure.step(f"Asserting element fill color: {0} to equal {1}")
     def assert_element_fill_color(self, by_locator, color):
         element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator))
         assert element.get_attribute("fill") == color
 
+    @allure.step(f"Asserting element displays: {0}")
     def assert_element_is_displayed(self, by_locator):
         element = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator)).is_displayed()
 
+    @allure.step(f"Asserting element size: {0} to equal {1}")
     def assert_element_size(self, by_locator, element_size):
         element = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(by_locator)).size
         assert element == element_size
         self.logger.info("Validation | Elements size is: " + str(element))
 
+    @allure.step(f"Asserting Get status for: {0} to equal {1}")
     def assert_GET_status(self, request_url, expected_GET_status_code):
         r = requests.get(request_url)
         assert r.status_code == expected_GET_status_code
         self.logger.info(f"Validation | {request_url} recieved expected GET status '{expected_GET_status_code}'")
 
+    @allure.step(f"Entering text: {1}, into {0}")
     def enter_text(self, by_locator, text):
         return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(by_locator)).send_keys(text)
         self.logger.info("Entered text: " + text)
@@ -66,10 +77,12 @@ class BasePage():
         page = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(Locators.PR_HEADER))
         self.logger.info("ENR Public Page has fully loaded")
 
+    @allure.step(f"Searching: {0}")
     def search(self, text):
         WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(Locators.SEARCH_TEXTBOX_PLACEHOLDER)).send_keys(text)
         self.logger.info("Entered " + text + " into the searchfield")
 
+    @allure.step("Asserting contest leaders")
     def assert_contest_leader(self, results1, results2):
         candidate1 = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(results1)).get_attribute("textContent")
         candidate2 = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(results2)).get_attribute("textContent")
